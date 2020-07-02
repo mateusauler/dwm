94662453c4bf813bfebd67587036aad54f84a0de
Mateus Auler <mateusauler@protonmail.com>
2020-07-02 01:04:51 -0300

    Removed vanity gaps

--- config.def.h
+++ config.def.h
@@ -3,11 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -101,10 +96,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_s,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_a,      incrgaps,       {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_a,      incrgaps,       {.i = -1 } },
-	{ MODKEY,                       XK_a,      togglegaps,     {0} },
-	{ MODKEY|AltMask,               XK_a,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
