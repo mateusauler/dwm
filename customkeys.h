1cc28473510ae2dd48d65cd7302c640bdb87105e
Mateus Auler <mateusauler@protonmail.com>
2020-07-09 08:54:57 -0300

    Added audio shortcuts and temporarely hardcoded font

---
+++ customkeys.h
@@ -0,0 +1,7 @@
+#define XK_AuLVolume	0x1008FF11   /* Volume control down        */
+#define XK_AuMute		0x1008FF12   /* Mute sound from the system */
+#define XK_AuRVolume	0x1008FF13   /* Volume control up          */
+#define XK_AuPlay		0x1008FF14   /* Start playing of audio >   */
+#define XK_AuStop		0x1008FF15   /* Stop playing audio         */
+#define XK_AuPrev		0x1008FF16   /* Previous track             */
+#define XK_AuNext		0x1008FF17   /* Next track                 */
