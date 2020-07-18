f4b261fb35d2740ac692d9f3b5c1cf6cf1dc29a1
Mateus Auler <mateusauler@protonmail.com>
2020-07-18 16:46:22 -0300

    Re-enabled resize hints

--- config.def.h
+++ config.def.h
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #include "gaplessgrid.c"
 
