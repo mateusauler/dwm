87e1f69c6b6626b3c28c1788b6d398b072d22c0e
Mateus Auler <mateusauler@protonmail.com>
2020-07-26 22:49:12 -0300

    Better debug script

--- debug.sh
+++ debug.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-pgrep Xephyr || Xephyr -ac -noreset -screen 1600x900 :1 &
+pgrep Xephyr || Xephyr -br -ac -resizeable -noreset -screen 1600x900 :1 &
 export DISPLAY=":1"
 make
 if [ $# -eq 1 ] && [ "$1" = "run" ]
 then
-	./dwm
+	./dwm &
 else
 	gdb ./dwm
 fi
