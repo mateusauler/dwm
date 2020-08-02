134be4e628e93301009e3952c919bf78cc358e08
Mateus Auler <mateusauler@protonmail.com>
2020-08-02 02:01:23 -0300

    dmenu command with history

--- config.def.h
+++ config.def.h
@@ -85,8 +85,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-//static const char *dmenucmd[]   = { "dmenu_run", "-wd", "900", "-m", dmenumon, "-fn", dmenufont, "-nb",  normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *dmenucmd[]   = { "dmenu_run", "-wd", "900", "-m", dmenumon, NULL };
+//static const char *dmenucmd[]   = { "dmenu_run_history", "-wd", "900", "-m", dmenumon, "-fn", dmenufont, "-nb",  normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[]   = { "dmenu_run_history", "-wd", "900", "-m", dmenumon, NULL };
 static const char *termcmd[]    = { "st", NULL };
 
 /*
