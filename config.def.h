e0bdbd379565a9c7763f655ce831a92405afdf74
Mateus Auler <mateusauler@protonmail.com>
2020-08-06 18:14:44 -0300

    Firefox class has lower case first letter

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                      instance    title           tags mask iscentered isfloating  isterminal  noswallow  monitor */
-	{ "Firefox",                  NULL,       NULL,           1 << 1,   1,         0,          0,          -1,        -1 },
+	{ "firefox",                  NULL,       NULL,           1 << 1,   1,         0,          0,          -1,        -1 },
 
 	{ "st",                       NULL,       NULL,           0,        1,         0,          1,          -1,        -1 },
 	{ "St",                       NULL,       NULL,           0,        1,         0,          1,          -1,        -1 },
