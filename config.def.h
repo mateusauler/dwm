ec969d83651228b9bc3bfc3c179062cb2658e2d2
Mateus Auler <mateusauler@protonmail.com>
2022-11-21 19:43:14 -0300

    config: Change the horizontal padding of the bar

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static int showbar                       = 1;	/* 0 means no bar */
 static int topbar                        = 1;	/* 0 means bottom bar */
 static const int user_bh                 = 26;	/* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static int vertpad                       = 5;	/* vertical padding of bar */
-static int sidepad                       = 2;	/* horizontal padding of bar */
+static int sidepad                       = 5;	/* horizontal padding of bar */
 static const char *fonts[]               = { "Droid Sans Mono Nerd Font:size=10", "monospace:size=10" };
 static const char dmenufont[]            = "monospace:size=12";
 static char normbgcolor[]                = "#222222";
